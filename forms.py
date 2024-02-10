ff8cd6f9914cff796606e13090f23197e17cd8bf
IDGS-803-17001472 <17001472@alumnos.utleon.edu.mx>
2024-02-09 18:12:27 -0600

    9/2/24 Resistencias flask

--- forms.py
+++ forms.py
@@ -7,3 +7,19 @@ class UsersForm(Form):
     x2=IntegerField('x2')
     y2=IntegerField('y2')
     res=0
+    
+class ResistenciasForm(Form):
+    colores=[("negro","Negro"),
+             ("cafe","Cafe"),
+             ("rojo","Rojo"),
+             ("naranja","Naranja"),
+             ("amarillo","Amarillo"),
+             ("verde","Verde"),
+             ("azul","Azul"),
+             ("violeta","Violeta"),
+             ("gris","Gris"),
+             ("blanco","Blanco")]
+    linea1=SelectField('linea1',choices=colores)
+    linea2=SelectField('linea2', choices=colores)
+    linea3=SelectField('linea3', choices=colores)
+    tolerancia=RadioField('tolerancia',choices=[("dorado","Dorado"),("plata","Plata")])
--- main.py
+++ main.py
@@ -85,6 +85,106 @@ def dist():
     return render_template("distancia.html", form=alum_form, res=res)
 
 
+    
+@app.route("/resistencias",methods=["GET","POST"])
+def resistencia():
+    res=""
+    tolerancia=""
+    toleranciaStr=""
+    resMin=""
+    linea1=""
+    linea2=""
+    linea3=""
+    resMax=""
+    restn_form = forms.ResistenciasForm(request.form)
+    if request.method=='POST':
+        linea1=restn_form.linea1.data
+        linea2=restn_form.linea2.data
+        linea3=restn_form.linea3.data
+        tolerancia=restn_form.tolerancia.data
+        if linea1=="negro":
+            nLinea1="0"
+        elif linea1=="cafe":
+            nLinea1="1"
+        elif linea1=="rojo":
+            nLinea1="2"
+        elif linea1=="naranja":
+            nLinea1="3"
+        elif linea1=="amarillo":
+            nLinea1="4"
+        elif linea1=="verde":
+            nLinea1="5"
+        elif linea1=="azul":
+            nLinea1="6"
+        elif linea1=="violeta":
+            nLinea1="7"
+        elif linea1=="gris":
+            nLinea1="8"
+        elif linea1=="blanco":
+            nLinea1="9"
+        
+        
+        if linea2=="negro":
+            nLinea2="0"
+        elif linea2=="cafe":
+            nLinea2="1"
+        elif linea2=="rojo":
+            nLinea2="2"
+        elif linea2=="naranja":
+            nLinea2="3"
+        elif linea2=="amarillo":
+            nLinea2="4"
+        elif linea2=="verde":
+            nLinea2="5"
+        elif linea2=="azul":
+            nLinea2="6"
+        elif linea2=="violeta":
+            nLinea2="7"
+        elif linea2=="gris":
+            nLinea2="8"
+        elif linea2=="blanco":
+            nLinea2="9"
+
+
+        if linea3=="negro":
+            nLinea3=1
+        elif linea3=="cafe":
+            nLinea3=10
+        elif linea3=="rojo":
+            nLinea3=100
+        elif linea3=="naranja":
+            nLinea3=1000
+        elif linea3=="amarillo":
+            nLinea3=10000
+        elif linea3=="verde":
+            nLinea3=100000
+        elif linea3=="azul":
+            nLinea3=1000000
+        elif linea3=="violeta":
+            nLinea3=10000000
+        elif linea3=="gris":
+            nLinea3=100000000
+        elif linea3=="blanco":
+            nLinea3=1000000000
+        toleranciaVal=0
+        toleranciaStr = tolerancia
+        print(tolerancia)
+        if tolerancia=="dorado":
+            toleranciaVal=0.05 
+        elif tolerancia=="plata":
+            toleranciaVal=0.1
+        
+        lineaTotal= nLinea1 + nLinea2
+
+
+        res=int(lineaTotal)*nLinea3
+        resMin=res*(1-toleranciaVal)
+        resMax=res*(1+toleranciaVal)
+        tolerancia = tolerancia + " " + str(toleranciaVal*100)+"%"
+        
+    return render_template("resistencias.html", form=restn_form, res=res, resMin=resMin,resMax=resMax, col1=linea1, col2=linea2, col3=linea3, tolerancia=tolerancia, toleranciaStr=toleranciaStr)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
